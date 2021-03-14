15924dd47c34a58f0dc9191bb713040342f463e6
Tasty <sapegin.petya@yandex.ru>
2021-03-14 13:05:41 +0400

    init: start virtual-piano task

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const loader = require('sass-loader');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const projectName = 'virtual-piano';
+
+module.exports = {
+	experiments: {
+		asset: true,
+	},
+	entry: `./${projectName}/src/app.js`,
+	output: {
+		path: path.resolve(__dirname, `${projectName}/dist`),
+		publicPath: '',
+		filename: 'bundle.js',
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: `./${projectName}/src/index.html`,
+		}),
+		new MiniCssExtractPlugin({
+			filename: 'style.css',
+		}),
+		new CleanWebpackPlugin(),
+		new CopyWebpackPlugin({
+			patterns: [
+				{
+					from: path.resolve(__dirname, `${projectName}/assets/`),
+					to: path.resolve(__dirname, `${projectName}/dist`),
+					noErrorOnMissing: true,
+				},
+			],
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+					},
+					'css-loader',
+					'sass-loader',
+				],
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(png|jpg|svg)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[path][name].[ext]',
+						},
+					},
+				],
+			},
+			{
+				test: /\.ttf$/,
+				type: 'asset/inline',
+			},
+		],
+	},
+	devServer: {
+		port: 3000,
+		open: true,
+		overlay: true,
+	},
+};
