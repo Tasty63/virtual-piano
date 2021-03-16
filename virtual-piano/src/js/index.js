f5d79991b43b98f18ae355250bf0a4664ea56a01
Tasty <sapegin.petya@yandex.ru>
2021-03-17 00:32:33 +0400

    feat: add mouseover keys sound

--- virtual-piano/src/js/index.js
+++ virtual-piano/src/js/index.js
@@ -1,6 +1,6 @@
-const keyboard = document.querySelector('.keyboard');
 const switchNotes = document.querySelector('.switch__notes');
 const switchLetters = document.querySelector('.switch__letters');
+const keyboard = document.querySelector('.keyboard');
 
 function getKeyElement(event) {
 	if (event.type == 'keydown' || event.type == 'keyup') {
@@ -14,10 +14,21 @@ function getKeyElement(event) {
 	return event.target.parentNode;
 }
 
+function isKeyNotFound(key) {
+	if (!key || !key.classList.contains('keyboard__key')) {
+		return true;
+	}
+	return false;
+}
+
 function playAudio(event) {
 	const key = getKeyElement(event);
-	const audio = new Audio();
+	if (isKeyNotFound(key)) {
+		return;
+	}
+
 	const sound = key.dataset.sound;
+	const audio = new Audio();
 
 	audio.src = `audio/${sound}.mp3`;
 	audio.currentTime = 0;
@@ -26,22 +37,36 @@ function playAudio(event) {
 
 function setActiveClass(event) {
 	const key = getKeyElement(event);
+	if (isKeyNotFound(key)) {
+		return;
+	}
+
 	key.classList.add('active');
 }
 
 function removeActiveClass(event) {
 	const key = getKeyElement(event);
+	if (isKeyNotFound(key)) {
+		return;
+	}
+
 	key.classList.remove('active');
 }
 
-keyboard.addEventListener('click', (event) => {
-	setActiveClass(event);
-	playAudio(event);
-});
-keyboard.addEventListener('transitionend', (event) => removeActiveClass(event));
+function playKey(event) {
+	if (!event.repeat) {
+		setActiveClass(event);
+		playAudio(event);
+	}
+}
 
-window.addEventListener('keydown', (event) => {
-	playAudio(event);
-	setActiveClass(event);
+keyboard.addEventListener('mousedown', (event) => {
+	playKey(event);
+	keyboard.addEventListener('mouseover', playKey);
 });
+keyboard.addEventListener('mouseout', (event) => removeActiveClass(event));
+keyboard.addEventListener('mouseup', (event) => removeActiveClass(event));
+
+window.addEventListener('keydown', (event) => playKey(event));
 window.addEventListener('keyup', (event) => removeActiveClass(event));
+window.addEventListener('mouseup', () => keyboard.removeEventListener('mouseover', playKey));
